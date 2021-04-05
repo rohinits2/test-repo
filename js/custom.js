4c7d6ccee1e7510db2789f1c093f883573812a92
rohini-inspheris <rohini@inspheris.com>
2021-04-05 09:21:23 +0545

    Mulesoft homepage

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$('.owl-carousel.select-track').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
